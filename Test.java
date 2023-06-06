7133ee549dc926b596d0468abd0c318605cebb65
wymc <wymc.mail@qq.com>
2023-06-06 14:11:18 +0800

    add file

---
+++ Test.java
@@ -0,0 +1,15 @@
+class Test {
+  private String a = "a";
+  private String b = "b";
+  private String c = "c";
+  private String d = "d";
+  private String e = "e";
+  private String f = "f";
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello World!");
+    System.out.println("Hello World");
+  }
+
+  
+}
