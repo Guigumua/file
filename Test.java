08925595358c751821994c2ac79a606a31e12b53
wymc <wymc.mail@qq.com>
2023-06-06 14:26:38 +0800

    add more fields

--- Test.java
+++ Test.java
@@ -5,11 +5,63 @@ class Test {
   private String d = "d";
   private String e = "e";
   private String f = "f";
+  private String g = "g";
+  private String h = "h";
+  private String i = "i";
+  private String j = "j";
+  private String k = "k";
+  private String l = "l";
+  private String m = "m";
+  private String n = "n";
+  private String o = "o";
+  private String p = "p";
+  private String q = "q";
+  private String r = "r";
+  private String s = "s";
+  private String t = "t";
+  private String u = "u";
+  private String v = "v";
+  private String w = "w";
+  private String x = "x";
+  private String y = "y";
+  private String z = "z";
+  private String aa = "aa";
+  private String ab = "ab";
+  private String ac = "ac";
+  private String ad = "ad";
+  private String ae = "ae";
+  private String af = "af";
+  private String ag = "ag";
+  private String ah = "ah";
+  private String ai = "ai";
+  private String aj = "aj";
+  private String ak = "ak";
+  private String al = "al";
+  private String am = "am";
+  private String an = "an";
+  private String ao = "ao";
+  private String ap = "ap";
+  private String aq = "aq";
+  private String ar = "ar";
+  private String as = "as";
+  private String at = "at";
+  private String au = "au";
+  private String av = "av";
+  private String aw = "aw";
+  private String ax = "ax";
+  private String ay = "ay";
+  private String az = "az";
+  private String ba = "ba";
+  private String bb = "bb";
+  private String bc = "bc";
+  private String bd = "bd";
+  private String be = "be";
+  private String bf = "bf";
+  private String bg = "bg";
 
   public static void main(String[] args) throws Exception {
     System.out.println("Hello World!");
     System.out.println("Hello World");
   }
-
   
 }
